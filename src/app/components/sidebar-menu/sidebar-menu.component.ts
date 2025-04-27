51a056a7f222e210d8d7366f3b697fd2bdfd555b
administrator <ms.pishbin2@gmail.com>
2025-04-27 23:01:25 +0330

    feat(ssr): add initial ssr setup for academy project

--- src/app/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,11 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-menu',
